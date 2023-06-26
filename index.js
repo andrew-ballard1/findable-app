958194b81524711f9567a11c48e3d2e3ccd88821
andrew-ballard1 <andrewj.ballard94@gmail.com>
2023-06-26 00:11:23 -0700

    Adding sentry initialization

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ import { SafeAreaProvider } from 'react-native-safe-area-context'
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 
+// Sentry.init({
+// 	enableInExpoDevelopment: true,
+// 	dsn: "https://bed29e1af6264ba78b40bae3b18695fc@o4505367858708480.ingest.sentry.io/4505407273369600",
+// 	// Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+// 	// We recommend adjusting this value in production.
+// 	tracesSampleRate: 1.0,
+// })
+
+
 const root = () => {
 	useEffect(() => {
 		Sentry.init({
@@ -19,10 +28,10 @@ const root = () => {
 			// We recommend adjusting this value in production.
 			// tracesSampleRate: 1.0,
 		})
-		
+
 	}, [])
 
-	const wrapped = Sentry.Browser.wrap(App)
+	const wrapped = Sentry.Native.wrap(App)
 	return (
 		<GlobalStateProvider>
 			<SafeAreaProvider>
@@ -32,5 +41,5 @@ const root = () => {
 	)
 }
 
-registerRootComponent(root)
-// registerRootComponent(Sentry.Browser.wrap(App))
+// registerRootComponent(root)
+registerRootComponent(Sentry.Native.wrap(App))
